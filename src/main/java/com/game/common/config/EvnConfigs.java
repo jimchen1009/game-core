f232255084b655f3be4862171f444061a2e9809d
chenjingjun <chenjingjun@aobi.com>
2020-08-19 16:33:55 +0800

    nothing important~

--- src/main/java/com/game/common/config/EvnConfigs.java
+++ src/main/java/com/game/common/config/EvnConfigs.java
@@ -3,7 +3,6 @@ package com.game.common.config;
 import com.typesafe.config.ConfigList;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -22,31 +21,34 @@ public class EvnConfigs implements IEvnConfig {
 
     @Override
     public IEvnConfig getConfig(String path){
-        return new EvnConfigs(Objects.requireNonNull(config.getConfig(path)));
+        return config.hasPath(path) ? new EvnConfigs(config.getConfig(path)) : null;
     }
 
     @Override
     public int getInt(String path){
-        return config.getInt(path);
+        return config.hasPath(path) ? config.getInt(path) : 0;
     }
 
     @Override
     public long getLong(String path){
-        return config.getLong(path);
+        return config.hasPath(path) ? config.getLong(path) : 0L;
     }
 
     @Override
     public String getString(String path) {
-        return config.getString(path);
+        return config.hasPath(path) ? config.getString(path) : null;
     }
 
     @Override
     public boolean getBoolean(String path) {
-        return config.getBoolean(path);
+        return config.hasPath(path) && config.getBoolean(path);
     }
 
     @Override
     public List<IEvnConfig> getConfigList(String path){
+        if (!config.hasPath(path)) {
+            return null;
+        }
         List<? extends com.typesafe.config.Config> configList = config.getConfigList(path);
         return configList.stream().map(EvnConfigs::new).collect(Collectors.toList());
     }
@@ -54,6 +56,9 @@ public class EvnConfigs implements IEvnConfig {
     @Override
     @SuppressWarnings("unchecked")
     public <T> List<T> getList(String path){
+        if (!config.hasPath(path)) {
+            return null;
+        }
         ConfigList configList = config.getList(path);
         return (List<T>)configList.unwrapped();
     }
